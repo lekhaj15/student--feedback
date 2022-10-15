6873eac3f59fb092f6a146a40cae7b3668866b6a
lekha15j <lekha15j@gmail.com>
2022-10-15 11:22:24 +0530

    .

--- app/Http/Controllers/institute/quiz/QuizAnswerController.php
+++ app/Http/Controllers/institute/quiz/QuizAnswerController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\institute\quiz;
 
 use App\Http\Controllers\Controller;
+use App\Models\institute\questions\Answer;
+use App\Models\institute\quiz\QuizAnswer;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
@@ -10,19 +12,48 @@ class QuizAnswerController extends Controller
 {
     // URI: /
     // SUM:
-    public function getIndex(Request $request): JsonResponse
+    public function getQuizAnswerIndex(Request $request): JsonResponse
     {
+        $institute_id=auth('institute')->id();
+//        dd($institute_id);
+        $quiz_answer=QuizAnswer::where('institute_id','=', $institute_id )
+
+            ->orderBy('id','DESC')
+
+            ->paginate(15);
         return response()->json([
-            '' => null,
-        ], JsonResponse::HTTP_OK);
+            'quiz_answer' => $quiz_answer,
+        ], Response::HTTP_OK);
+    }
+    public function getQuizAnswer(Request $request): JsonResponse
+    {
+        $institute_id=auth('institute')->id();
+//        dd($institute_id);
+        $quiz_answer=QuizAnswer::where('institute_id','=', $institute_id )
+
+            ->orderBy('id','DESC')
+
+            ->get();
+        return response()->json([
+            'quiz_answer' => $quiz_answer,
+        ], Response::HTTP_OK);
     }
 
     // URI: /
     // SUM:
-    public function postStore(Request $request): JsonResponse
+    public function postQuizAnswerStore(Request $request): JsonResponse
     {
+        $institute_id = auth('institute')->id();
+
+        $answer_name = $request->input('answer_name');
+
+        $quiz_answer =QuizAnswer::create([
+            'answer_name' => $answer_name,
+            'institute_id'=>$institute_id,
+
+        ]);
         return response()->json([
-            '' => null,
+            'quiz_answer' => $quiz_answer,
         ], JsonResponse::HTTP_CREATED);
     }
 
@@ -30,8 +61,11 @@ class QuizAnswerController extends Controller
     // SUM:
     public function getShow($id): JsonResponse
     {
+        $quiz_answer= QuizAnswer::where('id', '=',$id)
+            ->first();
+
         return response()->json([
-            '' => null,
+            'quiz_answer' => $quiz_answer,
         ], JsonResponse::HTTP_OK);
     }
 
@@ -46,19 +80,31 @@ class QuizAnswerController extends Controller
 
     // URI: /
     // SUM:
-    public function patchUpdate(Request $request, $id): JsonResponse
+    public function patchQuizAnswerUpdate(Request $request, $id): JsonResponse
     {
+        $answer_name = $request->input('answer_name');
+
+        QuizAnswer::toBase()->where([
+            ['id','=',$id],
+        ])
+
+            ->update([
+                'answer_name'=> $answer_name
+            ]);
+
         return response()->json([
-            '' => null,
+            'success' => 'update success'  ,
         ], JsonResponse::HTTP_ACCEPTED);
     }
 
     // URI: /
     // SUM:
-    public function deleteDestroy($id): JsonResponse
+    public function deleteQuizAnswerDestroy($id): JsonResponse
     {
+        $quiz_answer=QuizAnswer::where('id','=',$id)
+            ->delete();
         return response()->json([
-            '' => null,
+            'success' => 'delete success',
         ], JsonResponse::HTTP_NO_CONTENT);
     }
 }
