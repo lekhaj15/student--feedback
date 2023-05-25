d0ec56675bcee3c7463639c8375666be834368c4
lekha15j <lekha15j@gmail.com>
2023-05-25 21:02:03 +0530

    ,

---
+++ app/Models/student/Student.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models\student;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class Student extends Model
+{
+    use HasFactory;
+
+//    protected $table = '';
+
+    protected $fillable = [
+        'id',
+    ];
+
+    protected $hidden = [
+        'created_at', 'updated_at',
+    ];
+
+    public function scopegetTableName(): string
+    {
+        return $this->getTable();
+    }
+}
--- database/migrations/2022_09_09_100953_create_student_information_table.php
+++ database/migrations/2022_09_09_100953_create_student_information_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
         Schema::create('student_information', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('institute_id');
-
             $table->integer('student_id');
             $table->unsignedBigInteger('category_id');
             $table->unsignedBigInteger('subcategory_id');
